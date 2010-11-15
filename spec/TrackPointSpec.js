db7ea4e05d4f968626e9ce1f5f17e2f1fa9fb312
Jarkko Laine <jarkko@jlaine.net>
2010-11-15 11:47:53 -0600

    Added correction code for the multiple corrections case.

--- spec/TrackPointSpec.js
+++ spec/TrackPointSpec.js
@@ -30,12 +30,10 @@ describe('TrackPoint', function() {
       });
     });
     
-    describe("when only one correction", function() {
-      var correction;
-      
+    describe("when only one correction", function() {      
       beforeEach(function() {
         var point2 = track.addPoint(new TrackPoint([3,4]));
-        correction = point2.addCorrection([5,5]);
+        point2.addCorrection([5,5]);
       });
       
       it("should move the point exactly the same amount as the single correction", function() {
@@ -44,10 +42,17 @@ describe('TrackPoint', function() {
     });
     
     describe("when multiple corrections", function() {
-      xit("should move the point based on weighed average of the distance from the corrected point", function() {
+      beforeEach(function() {
+        var point2 = track.addPoint(new TrackPoint([3,4]));
+        point2.addCorrection([30,30]);
         
+        var point3 = track.addPoint(new TrackPoint([-6,-8]));
+        point3.addCorrection([-30,-30]);
+      });
+      
+      it("should move the point based on weighed average of the distance from the corrected point", function() {
+        expect(parseInt(point.correctedPosition().distanceFrom($V([10, 10])))).toEqual(0);
       });
     });
-    
   });
 });
--- src/TrackPoint.js
+++ src/TrackPoint.js
@@ -12,6 +12,21 @@ var TrackPoint = Class.create({
       return this.coordinates;
     } else if (this.track.corrections.size() == 1) {
       return this.coordinates.add(this.track.corrections.first().correction);
+    } else {
+      var self = this;
+      var total_dist = this.track.corrections.map(function(c) {
+        return self.coordinates.distanceFrom(c.point.coordinates)
+      }).inject(0, function(a,b) { 
+        return a + b;
+      });
+
+      var total = this.coordinates;
+
+      this.track.corrections.each(function(c) {
+        total = total.add(c.correction.multiply(1 - self.coordinates.distanceFrom(c.point.coordinates) / total_dist ));
+      });
+
+      return total;
     }
   }
 });
